60b3b8b8bdf3c4e035e710fde02fe1b87107f9c8
nuria <nuriarivasbotello@gmail.com>
2023-07-13 17:42:27 +0200

    +

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,6 @@ const buttonsElement = document.getElementById('buttons');
 
 let allTasks = [];
 
-const deleteTask = () => {};
-
 const listTasks = task => {
   const newTask = {
     id: Date.now(),
@@ -19,8 +17,27 @@ const listTasks = task => {
   appearTasks(allTasks);
 };
 
-const updateTask = id => {};
+const deleteTask = id => {
+  //Array original
+  // Si clico la X, me dará un id que coincidirá con el id de la tarea. Si estas ids no coinciden se meterán en un array nuevo que se volverá a pintar más tarde eliminado esta tarea.
+  //Por eso luego digo que allTasks es igual al nuevo array (newTasks).
+  const newTasks = [];
+  for (const task of allTasks) {
+    if (task.id !== id) {
+      newTasks.push(task);
+    }
+  }
+  allTasks = newTasks;
+
+  //allTasks = allTasks.filter(task => task.id !== id); Este proceso es más rápido que lo de arriba, usando filter me busca la condición que le pongo a continuación (Lo mismo explicado arriba).
+
+  appearTasks(allTasks);
+  console.log(id);
+  console.log(allTasks);
+  // Array pero sin una tarea
+};
 const taskElement = task => {
+  tasksElement.textContent = '';
   const newDiv = document.createElement('div');
   newDiv.classList.add('task');
 
@@ -35,7 +52,8 @@ const taskElement = task => {
   const newButton = document.createElement('button');
   newButton.textContent = 'X';
   newDiv.append(newButton);
-  newButton.addEventListener('click', deleteTask);
+
+  newButton.addEventListener('click', () => deleteTask(task.id));
   tasksElement.append(newDiv);
   return newDiv;
 };
